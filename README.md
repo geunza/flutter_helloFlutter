1e0e560fc85c9693969ca609368ceaf025e7953c
geunza <safa940812@gmail.com>
2023-08-07 01:32:45 +0900

    레이아웃 작업

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 ## Hello Flutter
 
 - 위젯으로 시작해서 위젯으로 끝났다.
+
+# 230806
+
+## 레이아웃 잡기
+
+- Scaffold : 기본 뼈대
+- Column : 수직 레이아웃
+- SizedBox : 공간용 레이아웃 박스
+- Container : 공간용 레이아웃 박스(div in html)
+- Row : 수평 레이아웃
+- Text : 텍스트 레이아웃
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,102 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String name;
+
+  Player(this.name);
+}
+
 void main() {
-  runApp(App());
+  var nico = Player("nico");
+  print(nico);
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter!"),
-          centerTitle: true,
-          elevation: 5,
-        ),
-        body: Center(
-          child: Text("Hello World!"),
-        ),
-        backgroundColor: Colors.blue,
-      ),
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 0.8),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text("Total Balance",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 22,
+                    )),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text("\$5 194 482",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 48,
+                      fontWeight: FontWeight.w600,
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF2B33A),
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 50,
+                          ),
+                          child: Text(
+                            "Transfer",
+                            style: TextStyle(
+                              fontSize: 22,
+                            ),
+                          ),
+                        )),
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
